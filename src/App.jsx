27b6e18d65e4f252602a22a0c0981040297b7291
Grant Greer <viridiang93@gmail.com>
2023-02-19 00:03:38 -0800

    feat: update deps

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Pokedex from './Pokedex'
 import PokemonInfoTile from './components/PokemonInfoTile'
 
 const App = () => {
+  const routes = [
+    { path: "/", element: <Pokedex /> },
+    { path: "/pokemon/:pokemonId", element: <PokemonInfoTile /> },
+  ];
+
   return (
     <div className='App'>
-      <Router>
-        <Switch>
-          <Route path='/pokemon/:id' component={PokemonInfoTile} />
-          <Route path='/' component={Pokedex} />
-        </Switch>
-      </Router>
+     <Router>
+      <Routes>
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+      </Routes>
+    </Router>
     </div>
   );
 };
--- src/services/getPokemons.js
+++ src/services/getPokemons.js
@@ -1,1 +1,9 @@
-export const getPokemons = (offset) => fetch(`https://pokeapi.co/api/v2/pokemon?limit=9&offset=${offset}`).then((response) => response.json());
+import axios from "axios";
+
+
+// Function to get the list of Pokemon
+export const getPokemons = async (offset) => {
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=9&offset=${offset}`;
+  const { data } = await axios.get(url);
+  return data;
+};
